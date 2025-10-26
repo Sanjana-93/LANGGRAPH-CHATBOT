1ae5d5e3cd58fdcb7abdf91f31bcb2d422ff20ff
Sanjana-93 <ds4454301@gmail.com>
2025-10-27 00:47:57 +0530

    Add files via upload

---
+++ langgraph_backend.py
@@ -0,0 +1,37 @@
+from langgraph.graph import StateGraph,START,END
+from typing_extensions import TypedDict,Annotated
+from langchain_core.messages import BaseMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model= 'gemini-2.5-flash',temperature=0.7)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage],add_messages] 
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response= llm.invoke(messages)
+    return {"messages": [response]}  
+    #checkpointer
+checkpointer = InMemorySaver() 
+graph = StateGraph(ChatState)
+graph.add_node("chat_node",chat_node)
+graph.add_edge(START,"chat_node")
+graph.add_edge("chat_node",END)
+chatbot = graph.compile(checkpointer = checkpointer)
+
+# for message_chunk, metadata in chatbot.stream(
+#     {'messages': [HumanMessage(content='what is the recipe to make pasta?')]},
+#     config = {'configurable': {'thread_id': 'thread-1'}},
+#     stream_mode='message'
+# ):
+    
+#     if message_chunk.content:
+#         print(message_chunk.content, end=" ",flush=True)
+
+
+    
