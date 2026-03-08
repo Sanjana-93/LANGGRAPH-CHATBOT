225ddf502e1c48dcad9e6bf35d171ec19801d432
Sanjana-93 <ds4454301@gmail.com>
2026-03-08 22:09:10 +0530

    Update streamlit_frontend_streaming.py

--- streamlit_frontend_streaming.py
+++ streamlit_frontend_streaming.py
@@ -33,11 +33,13 @@ if user_input:
     with st.chat_message('assistant'):
        
        ai_message = st.write_stream(
-            message_chunk.content for message_chunk, metadata in chatbot.stream in chatbot.stream(
-                {'messages': [HumanMessage(content=user_input)]},
-                config = {'configurable': {'thread_id': 'thread-1'}},
-                stream_mode='message'
-           )
-       )
+            message_chunk.content
+        for message_chunk, metadata in chatbot.stream(
+            {"messages": [HumanMessage(content=user_input)]},
+            config=CONFIG,
+            stream_mode="messages"
+        )
+    )
        
+
     st.session_state['message_history'].append({'role': 'assistant','content': ai_message})
